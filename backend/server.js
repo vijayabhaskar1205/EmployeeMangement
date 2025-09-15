2d6e5ca20d1e39e07b2dd89c76ee70fcfaa64b88
vijayabhaskar1205 <vijayabhaskar2035@gmail.com>
2025-09-15 12:03:16 +0530

    second

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,17 @@
 const express=require('express')
 const cors=require('cors')
 const app=express()
-
 app.use(express.json())
-app.use(cors())
+app.use(cors({ origin: "https://employee-mangement-plum.vercel.app" }));
 
 const emprouter=require('./routes/employeeroute')
 app.use('/image',express.static('image'))
 app.use('/employee',emprouter)
-
+db.sequelize.sync({ alter: true })  
+  .then(() => {
+    console.log("Database & tables created!");
+  })
+  .catch(err => console.log("Error creating tables:", err));
 app.listen(7500,()=>{
     console.log("server created")
 })
