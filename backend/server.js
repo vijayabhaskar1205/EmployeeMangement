62a90d2b81a931eb6f3cb6b8a2d54c21ef5566ad
vijayabhaskar1205 <vijayabhaskar2035@gmail.com>
2025-09-15 12:49:32 +0530

    eigth

--- backend/server.js
+++ backend/server.js
@@ -3,23 +3,7 @@ const cors=require('cors')
 const app=express()
 const db = require('./models');
 app.use(express.json())
-const allowedOrigins = [
-  "https://employee-mangement-plum.vercel.app",
-  "https://employee-mangement-94ty1suqy-vijayabhaskar1205s-projects.vercel.app"
-];
-
-app.use(cors({
-  origin: function(origin, callback){
-  
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      const msg = 'CORS policy does not allow access from this origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-  credentials: true
-}));
+app.use(cors({ origin: "https://employee-mangement-plum.vercel.app" }));
 
 const emprouter=require('./routes/employeeroute')
 app.use('/image',express.static('image'))
